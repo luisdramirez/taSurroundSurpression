0cb0055078f599c8e94b969effa6de652f95d7f2
luisdramirez <ldr285@nyu.edu>
2017-07-13 12:13:06 -0400

    analysis code

--- analyzeSurroundSurpression.m
+++ analyzeSurroundSurpression.m
@@ -1,1 +1,19 @@
 %%% analyzeSurroundSurpression
+function analyzeSurroundSurpression(subject, runNumber)
+
+expDir = pwd;
+dataDir = 'data';
+cd(dataDir)
+
+if exist(['vTA_surrSuppression_', subject, '.mat'],'file');
+    load(['vTA_surrSuppression_', subject, '.mat']);
+else
+    error('Data file does not exist.')
+end
+
+estimatedContrast = theData(runNumber).data.estimatedContrast;
+differenceContrast = theData(runNumber).data.differenceContrast;
+responseTime = theData(runNumber).data.responseTime;
+
+
+return
